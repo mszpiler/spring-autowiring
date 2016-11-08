00a637bcfbbd58205cef0b2a3aa3e42383fd9b59
Mariusz Szpiler <mariusz.szpiler@smtsoftware.com>
2016-11-08 12:13:14 +0100

    @Resource - search byName, but if fails then search byType @Inject - search byType

--- src/main/java/com/enotion/elearning/spring/beans/ExampleJoeDefaultBean.java
+++ src/main/java/com/enotion/elearning/spring/beans/ExampleJoeDefaultBean.java
@@ -2,9 +2,24 @@ package com.enotion.elearning.spring.beans;
 
 import com.enotion.elearning.spring.services.BarService;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
 public class ExampleJoeDefaultBean {
 
     private BarService barService;
+
+    /**
+     * @Inject search bean byType - BarService in spring configuration has two beans, so there is a problem with injection
+     */
+//    @Inject
+//    private BarService barServiceInject;
+
+    /**
+     * @Resource search bean byName
+     */
+    @Resource
+    private BarService barServiceThree;
 
     public void execute() {
 
@@ -15,6 +30,8 @@ public class ExampleJoeDefaultBean {
         } else {
             System.out.println("Autowire is not attached as a xml config nor @Autowired annotation so service is null.");
         }
+
+        barServiceThree.bar();
     }
 
     public void setBarService(BarService barService) {
